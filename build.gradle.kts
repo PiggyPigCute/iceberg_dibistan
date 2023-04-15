35ec361bd3daf36ebdeef78b116f6645142da173
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2023-04-15 19:52:44 +0200

    rapide modificaiton

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,4 +32,5 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("org.javacord:javacord:3.8.0")
 }
--- src/main/kotlin/com/dirtybiologistan/iceberg/Application.kt
+++ src/main/kotlin/com/dirtybiologistan/iceberg/Application.kt
@@ -1,11 +1,17 @@
 package com.dirtybiologistan.iceberg
 
+import com.dirtybiologistan.iceberg.plugins.configureRouting
+import com.dirtybiologistan.iceberg.plugins.configureSerialization
+import com.dirtybiologistan.iceberg.plugins.configureSockets
+import com.dirtybiologistan.iceberg.plugins.configureTemplating
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.tomcat.*
-import com.dirtybiologistan.iceberg.plugins.*
 
 fun main() {
+    println("Hello, world!")
+    println("Plouf, l'iceberg est là !")
+
     embeddedServer(Tomcat, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
