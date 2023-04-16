acfcb7a52491532dbe14bc0286887bbe2564e1cc
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2023-04-16 11:28:38 +0200

    suite

--- src/main/kotlin/com/dirtybiologistan/iceberg/Application.kt
+++ src/main/kotlin/com/dirtybiologistan/iceberg/Application.kt
@@ -7,10 +7,20 @@ import com.dirtybiologistan.iceberg.plugins.configureTemplating
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.tomcat.*
+import org.javacord.api.DiscordApiBuilder
+import org.javacord.api.entity.intent.Intent
+
 
 fun main() {
     println("Hello, world!")
     println("Plouf, l'iceberg est là !")
+
+    println(System.getenv("token_iceberg"))
+
+    val api = DiscordApiBuilder()
+        .setToken(System.getenv("token_iceberg"))
+        .addIntents(Intent.MESSAGE_CONTENT)
+        .login().join()
 
     embeddedServer(Tomcat, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
