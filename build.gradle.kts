bc43188c6da4ce62c9ed9bd154d34d218352db18
Alexiscomete <wallan3100@gmail.com>
2023-05-25 16:27:35 +0100

    fish début

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,15 +22,15 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-websockets-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-host-common-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-freemarker-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-tomcat-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-core-jvm:2.3.0")
+    implementation("io.ktor:ktor-server-websockets-jvm:2.3.0")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:2.3.0")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:2.3.0")
+    implementation("io.ktor:ktor-server-host-common-jvm:2.3.0")
+    implementation("io.ktor:ktor-server-freemarker-jvm:2.3.0")
+    implementation("io.ktor:ktor-server-tomcat-jvm:2.3.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
     implementation("org.javacord:javacord:3.8.0")
+    testImplementation("io.ktor:ktor-server-tests-jvm:2.3.0")
 }
---
+++ src/main/resources/static/fishinfo.js
@@ -0,0 +1,23 @@
+let current_fish = null
+let overlay = document.getElementById("overlay")
+
+for (let fish of document.getElementsByClassName("fish")) {
+    fish.onclick = () => {
+        let w = document.getElementById("window_" + fish.id).classList
+        if (w.contains("opened")) {
+            w.remove("opened")
+            overlay.classList.remove("show")
+        } else {
+            w.add("opened")
+            overlay.classList.add("show")
+        }
+    }
+}
+
+overlay.onclick = () => {
+    overlay.classList.remove("show")
+    if (current_fish != null) {
+        current_fish.remove("opened")
+        current_fish = null
+    }
+}
