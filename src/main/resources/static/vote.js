cdf2f5dc5c12c87c34d197591a1ff072a0632a6b
Alexiscomete <wallan3100@gmail.com>
2023-06-17 18:22:00 +0100

    Début websocket

--- src/main/resources/static/vote.js
+++ src/main/resources/static/vote.js
@@ -1,5 +1,24 @@
-// for (let element of document.getElementsByClassName("propimg")) {
-//     element.onerror = () => {
-//         element.src = '/static/default.png'
-//     }
-// }
+let url = document.URL
+let domain = url.split('/')[2]
+let ws = new WebSocket('ws://' + domain + '/ws')
+
+let prop0 = document.getElementById("prop0")
+let prop1 = document.getElementById("prop1")
+
+prop0.onclick = function () {
+    ws.send(JSON.stringify({
+        type: 'vote',
+        vote: 0
+    }))
+}
+
+prop1.onclick = function () {
+    ws.send(JSON.stringify({
+        type: 'vote',
+        vote: 1
+    }))
+}
+
+ws.onmessage = function (event) {
+    console.log(event.data)
+}
